c0818ff7006f1d278e87a9337abb80f912cdc708
Ezekiel Templin <zeke@symops.io>
2021-03-17 07:01:29 -0700

    Set template handle to sym:template:approval:1.0.0

--- samples/6-test-all/main.tf
+++ samples/6-test-all/main.tf
@@ -67,7 +67,7 @@ resource "sym_flow" "this" {
   name  = "sso_access"
   label = "SSO Access"
 
-  template       = "sym:template:approval:1.0"
+  template       = "sym:template:approval:1.0.0"
   implementation = "impl.py"
 
   environment = {
