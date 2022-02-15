bf83e58c7c4344e053174e9f085b873c570d2c74
Ari <Arianna2028@users.noreply.github.com>
2022-02-15 13:58:34 -0500

    add more config around initial integration acceptance test (#86)
    * add more config around initial integration acceptance test
    
    * Update sym/provider/provider_test.go
    
    Co-authored-by: Komal <komalsali@gmail.com>
    
    * Update sym/provider/provider_test.go
    
    Co-authored-by: Komal <komalsali@gmail.com>
    
    * maybe this fixes lint?
    
    Co-authored-by: Komal <komalsali@gmail.com>

--- sym/provider/provider_test.go
+++ sym/provider/provider_test.go
@@ -1,6 +1,11 @@
 package provider
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
 
 var testAccProviderFactories map[string]func() (*schema.Provider, error)
 var testAccProvider *schema.Provider
@@ -11,5 +16,23 @@ func init() {
 		"sym": func() (*schema.Provider, error) {
 			return testAccProvider, nil
 		},
+	}
+}
+
+// testAccPreCheck should be used as a PreCheck to ensure the current
+// environment is configured for each acceptance test.
+func testAccPreCheck(t *testing.T) {
+	if os.Getenv("SYM_JWT") == "" {
+		t.Fatal("SYM_JWT must be set for acceptance tests")
+	}
+
+	// TODO: If we want to do tests against production we'll need to
+	// remove this check. We probably want a combination of API URL
+	// + OrgSlug checks (e.g. staging must use org slug "staging-test")?
+	apiUrl := os.Getenv("SYM_API_URL")
+
+	// Unset SYM_API_URL defaults to production
+	if apiUrl == "" || apiUrl == "https://api.symops.com/api/v1" {
+		t.Fatal("Acceptance tests must not point to production")
 	}
 }
