74adecd666597f1a39da91ea482493af811ec990
Ari <Arianna2028@users.noreply.github.com>
2022-02-15 13:45:43 -0500

    Update sym/provider/provider_test.go
    Co-authored-by: Komal <komalsali@gmail.com>

--- sym/provider/provider_test.go
+++ sym/provider/provider_test.go
@@ -1,9 +1,10 @@
 package provider
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"os"
 	"testing"
+	
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 var testAccProviderFactories map[string]func() (*schema.Provider, error)
