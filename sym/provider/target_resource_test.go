b8f540b14ef16cb6cf77211da65a79aa6fab46c4
Ari <Arianna2028@users.noreply.github.com>
2022-02-15 16:53:03 -0500

    test: sym_target resource

---
+++ sym/provider/target_resource_test.go
@@ -0,0 +1,64 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+
+const arnPrefix = "arn:aws:sso:::permissionSet/ins-abcdefghijklmnop/"
+
+
+func TestAccSymTarget_awsSso(t *testing.T) {
+	createData := BuildTestData(t, "target")
+	updateData := BuildTestData(t, "updated-target")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: awsSsoTarget(createData, "My Target", "foo", "012345678910"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("sym_target.sso", "type", "aws_sso_permission_set"),
+					resource.TestCheckResourceAttr("sym_target.sso", "name", createData.ResourceName),
+					resource.TestCheckResourceAttr("sym_target.sso", "label", "My Target"),
+					resource.TestCheckResourceAttr("sym_target.sso", "settings.permission_set_arn", arnPrefix + "foo"),
+					resource.TestCheckResourceAttr("sym_target.sso", "settings.account_id", "012345678910"),
+				),
+			},
+			{
+				Config: awsSsoTarget(updateData, "Other Target", "bar", "000000000000"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("sym_target.sso", "type", "aws_sso_permission_set"),
+					resource.TestCheckResourceAttr("sym_target.sso", "name", updateData.ResourceName),
+					resource.TestCheckResourceAttr("sym_target.sso", "label", "Other Target"),
+					resource.TestCheckResourceAttr("sym_target.sso", "settings.permission_set_arn", arnPrefix + "bar"),
+					resource.TestCheckResourceAttr("sym_target.sso", "settings.account_id", "000000000000"),
+				),
+			},
+		},
+	})
+}
+
+func awsSsoTarget(t TestData, label string, arnSuffix string, accountId string) string {
+	return fmt.Sprintf(`
+provider "sym" {
+	org = "%[1]s"
+}
+
+resource "sym_target" "sso" {
+	type = "aws_sso_permission_set"
+	name = "%[2]s"
+	label = "%[3]s"
+	
+	settings = {
+		permission_set_arn = "%[4]s%[5]s"
+		account_id = "%[6]s"
+	}
+}
+
+`, t.OrgSlug, t.ResourceName, label, arnPrefix, arnSuffix, accountId)
+}
