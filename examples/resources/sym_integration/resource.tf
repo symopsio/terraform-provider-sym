42d22713c1e41e8d633ff2c3046374a5f1abfbc8
Komal <komalsali@gmail.com>
2022-07-08 11:10:35 -0700

    Log Destination and Integration Examples (#117)

--- examples/resources/sym_integration/resource.tf
+++ examples/resources/sym_integration/resource.tf
@@ -17,3 +17,44 @@ resource "sym_integration" "slack" {
 
   external_id = "T1234567"
 }
+
+# okta integration
+resource "sym_integration" "okta" {
+  type        = "okta"
+  name        = "main-okta-integration"
+  external_id = "dev-12345.okta.com"
+
+  settings = {
+    # `type=okta` sym_integrations have a required setting `api_token_secret`,
+    # which must point to a sym_secret referencing your Okta API Key
+    api_token_secret = "xxx12345abcde"
+  }
+}
+
+# github integration
+resource "sym_integration" "github" {
+  type        = "github"
+  name        = "main-okta-integration"
+  external_id = "my-github-org" # github org slug
+
+  settings = {
+    # `type=github` sym_integrations have a required setting `api_token_secret`,
+    # which must point to a sym_secret referencing your Github API Key
+    api_token_secret = "xxx12345abcde"
+  }
+}
+
+# runtime permission context integration
+resource "sym_integration" "runtime_context" {
+  type = "permission_context"
+  name = "aws-flow-context-test"
+  label = "Runtime context"
+  external_id = "123456789012"
+
+  settings = {
+    cloud       = "aws"
+    external_id = "1478F2AD-6091-41E6-B3D2-766CA2F173CB" # optional
+    region      = "us-east-1"
+    role_arn    = "arn:aws:iam::123456789012:role/sym/RuntimeConnectorRole"
+  }
+}
