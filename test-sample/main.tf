3bb86879c4a87a3537ad61d6e68260d85201ad5d
Ari <Arianna2028@users.noreply.github.com>
2022-10-25 15:47:40 -0400

    chore: tf fmt

--- test-sample/main.tf
+++ test-sample/main.tf
@@ -24,15 +24,15 @@ resource "sym_flow" "this" {
   environment_id = sym_environment.this.id
 
   params {
-    schedule_deescalation = true
+    schedule_deescalation   = true
     allow_guest_interaction = true
-    allowed_sources = ["api", "slack"]
+    allowed_sources         = ["api", "slack"]
 
     prompt_field {
-      name = "reasonssss"
-      type = "string"
+      name     = "reasonssss"
+      type     = "string"
       required = true
-      label = "Reason"
+      label    = "Reason"
     }
 
     prompt_field {
@@ -47,10 +47,10 @@ resource "sym_flow" "this" {
 resource "sym_strategy" "this" {
   type = "aws_sso"
 
-  name = "strategy-v2tf"
-  label = "V2 Provider Test"
+  name           = "strategy-v2tf"
+  label          = "V2 Provider Test"
   integration_id = sym_integration.runtime_context.id
-  targets = [ sym_target.prod_break_glass.id, sym_target.sandbox_break_glass.id ]
+  targets        = [sym_target.prod_break_glass.id, sym_target.sandbox_break_glass.id]
 
   settings = {
     instance_arn = "arn:aws:::instance/ssoinst-abcdefghi12314135325"
@@ -60,31 +60,31 @@ resource "sym_strategy" "this" {
 resource "sym_target" "prod_break_glass" {
   type = "aws_sso_permission_set"
 
-  name = "prod-target-v2tf"
+  name  = "prod-target-v2tf"
   label = "V2 Provider Test Prod"
 
   settings = {
     permission_set_arn = "arn:aws:sso:::permissionSet/ins-abcdefghijklmnop/ps-2"
-    account_id = "012345678910"
+    account_id         = "012345678910"
   }
 }
 
 resource "sym_target" "sandbox_break_glass" {
   type = "aws_sso_permission_set"
 
-  name = "sandbox-target-v2tf"
+  name  = "sandbox-target-v2tf"
   label = "V2 Provider Test Sandbox"
 
   settings = {
     permission_set_arn = "arn:aws:sso:::permissionSet/ins-abcdefghijklmnop/ps-2"
-    account_id = "012345678910"
+    account_id         = "012345678910"
   }
 }
 
 resource "sym_environment" "this" {
-  name       = "environment-v2tf"
-  label = "V2 Provider Test"
-  runtime_id = sym_runtime.this.id
+  name            = "environment-v2tf"
+  label           = "V2 Provider Test"
+  runtime_id      = sym_runtime.this.id
   error_logger_id = sym_error_logger.slack_logger.id
 
   integrations = {
@@ -100,23 +100,24 @@ resource "sym_error_logger" "slack_logger" {
 resource "sym_integration" "slack" {
   type = "slack"
 
-  name = "slack-v2tf"
-  label = "V2 Provider Test Slack"
+  name        = "slack-v2tf"
+  label       = "V2 Provider Test Slack"
   external_id = "T123ABC"
 }
 
 resource "sym_runtime" "this" {
   name       = "runtime-v2tf"
-  label = "V2 Provider Test"
+  label      = "V2 Provider Test"
   context_id = sym_integration.runtime_context.id
 }
 
 resource "sym_integration" "runtime_context" {
   type = "permission_context"
 
-  name = "runtime-context-v2tf"
-  label = "V2 Provider Test Runtime Context"
+  name        = "runtime-context-v2tf"
+  label       = "V2 Provider Test Runtime Context"
   external_id = "123456789012"
+
   settings = {
     cloud       = "aws"
     external_id = "1478F2AD-6091-41E6-B3D2-766CA2F173CB"
